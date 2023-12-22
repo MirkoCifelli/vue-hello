7e7275f066250b79cfdbbfef0b93f34d43984e59
MirkoCifelli <ciffo4@hotmail.it>
2023-12-22 14:36:05 +0100

    Add img

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,11 @@
 Vue.createApp({
     data() {
       return {
-        message: 'Hello World!'
+        message: 'Hello World!',
+        urlImg: 'https://picsum.photos/200/300'
       }
     }
 }).mount('#app')
+
+
+
